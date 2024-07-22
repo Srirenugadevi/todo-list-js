18934a41fa9fc0ca359809d9e499a5de8dcbea51
Srirenugadevi <114763486+Srirenugadevi@users.noreply.github.com>
2024-07-22 09:28:54 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,17 @@
+
+function getinput(){
+    var add=document.getElementById("listtask")
+    var input=document.getElementById("myinput")
+        var litem=document.createElement("li")
+        litem.innerHTML=input.value+""+"<button onclick='del(event)'>Delete</button>"
+        if(input.value==null||input.value==""){
+            alert("Enter some text");
+        }
+        else{
+        add.append(litem);
+        }
+}
+
+function del(event){
+    event.target.parentElement.remove();
+}
